1e47e0f9b250d267558f488194db0e68d075b380
Bruno Rossi <brunorossiweb2000@yahoo.com>
2012-02-17 00:57:26 +0100

    Cleaned Index Controller

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,14 +5,11 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
     }
 
     public function indexAction()
     {
-    	echo "test";
     }
-
 
 }
 
