07943b2dbaa1565003cd47a9688f1918e09c7ccc
Bruno Rossi <brunorossiweb2000@yahoo.com>
2012-02-21 00:21:31 +0100

    Upload of the Doctrine Extension Library Gedmo

---
+++ library/Doctrine/Gedmo/Mapping/Driver/GedmoAnnotations.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+* Contains all annotations for extensions
+* NOTE: should be included with require_once
+*
+* @author Gediminas Morkevicius <gediminas.morkevicius@gmail.com>
+* @package Gedmo.Mapping.Annotation
+* @link http://www.gediminasm.org
+* @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+*/
+
+include __DIR__.'/../Annotation/Language.php';
+include __DIR__.'/../Annotation/Locale.php';
+include __DIR__.'/../Annotation/Loggable.php';
+include __DIR__.'/../Annotation/Slug.php';
+include __DIR__.'/../Annotation/SlugHandler.php';
+include __DIR__.'/../Annotation/SlugHandlerOption.php';
+include __DIR__.'/../Annotation/SortableGroup.php';
+include __DIR__.'/../Annotation/SortablePosition.php';
+include __DIR__.'/../Annotation/Timestampable.php';
+include __DIR__.'/../Annotation/Translatable.php';
+include __DIR__.'/../Annotation/TranslationEntity.php';
+include __DIR__.'/../Annotation/Tree.php';
+include __DIR__.'/../Annotation/TreeClosure.php';
+include __DIR__.'/../Annotation/TreeLeft.php';
+include __DIR__.'/../Annotation/TreeLevel.php';
+include __DIR__.'/../Annotation/TreeParent.php';
+include __DIR__.'/../Annotation/TreeRight.php';
+include __DIR__.'/../Annotation/TreeRoot.php';
+include __DIR__.'/../Annotation/Versioned.php';
